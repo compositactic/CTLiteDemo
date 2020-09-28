5b9440af2349a9296b6fadbc43ab49e1e8820933
Matt J. Crouch <matt_crouch@hotmail.com>
2020-09-28 12:41:15 -0400

    Fixing encoding on bat file

--- CTLite.Tools.CTGen/QUICKSTART.bat
+++ CTLite.Tools.CTGen/QUICKSTART.bat
@@ -1,4 +1,4 @@
-dotnet run CTGen -r "NorthwindDemo\NorthwindApplications" -a webapi -p -c -cc -cr -csvc -sc -sc -sr -srcdb -mcs "Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=SSPI;" -dbcs "Data Source=(LocalDb)\MSSQLLocalDB;Integrated Security=SSPI;"
+CTGen -r "NorthwindDemo\NorthwindApplications" -a webapi -p -c -cc -cr -csvc -sc -sc -sr -srcdb -mcs "Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=SSPI;" -dbcs "Data Source=(LocalDb)\MSSQLLocalDB;Integrated Security=SSPI;"
 cd NorthwindDemo
 dotnet build
 cd NorthwindApplication.WebApi
